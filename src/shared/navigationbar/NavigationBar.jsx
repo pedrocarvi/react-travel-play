ce6590898c0a839a0f0ea6f596855f5bca8dffb1
Pedro Carvi <pedro.carvi-11@outlook.es>
2023-10-03 19:24:22 -0300

    front-031023

---
+++ src/shared/navigationbar/NavigationBar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React from 'react'
+
+const NavigationBar = () => {
+    return (
+        <div>
+            <Navbar bg="primary" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home"> Travel and Play! </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home"> Inicio </Nav.Link>
+                        <Nav.Link href="#features"> Publicaciones </Nav.Link>
+                        <Nav.Link href="#pricing"> Iniciar sesion </Nav.Link>
+                        <Nav.Link href="#"> Registrarse </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavigationBar
