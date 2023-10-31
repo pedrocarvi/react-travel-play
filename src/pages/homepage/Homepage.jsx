a0c9cd1b45fc147e6fa09499be2eb021d486240e
Pedro Carvi <pedro.carvi-11@outlook.es>
2023-10-31 19:13:56 -0300

    cambios

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavigationBar from '../../shared/navigationbar/NavigationBar'
+import Post from '../../components/post/Post'
 
 const Homepage = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
             <h1>Homepage</h1>
+            <Post></Post>
         </div>
     )
 }
